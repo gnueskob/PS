c1739e3c6996336716f60c2b2618aa1b5541e6d6
gnues <gnueskob@gmail.com>
2021-02-22 01:08:48 +0900

    Modify template

--- baekjoon/_template.cpp
+++ baekjoon/_template.cpp
@@ -1,7 +1,6 @@
 // problem_name
 // url
 #include<bits/stdc++.h>
-using namespace std;
 
 // #include "_fileRead.hpp"
 // GNUES::FileRead f("__.txt");
@@ -12,7 +11,7 @@ template <typename T> inline
 void writeInt(T x,char e) {if(x<0)writeChar(45),x=-x;char s[24]{};int l=0;while(x||!l)s[l++]=x%10,x/=10;while(l--)writeChar(s[l]+48);if(e)writeChar(e);}
 
 int main() {
-  ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+  std::ios::sync_with_stdio(false); std::cin.tie(NULL); std::cout.tie(NULL);
 
   // freopen("./input/1918.txt", "r", stdin);
   std::ifstream file("./baekjoon/input/10951.txt");
